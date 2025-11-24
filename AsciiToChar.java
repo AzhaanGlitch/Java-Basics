e6a4a696bf2a3826fc4d8b0955072502e07a0091
Azhaan Ali Siddiqui <azhaanalisiddiqui15@gmail.com>
2025-11-24 20:40:33 +0530

    ascii to char java

---
+++ AsciiToChar.java
@@ -0,0 +1,12 @@
+public class AsciiToChar {
+
+    public static void main(String[] args) {
+        int asciiValue1 = 65; 
+        char character1 = (char) asciiValue1;
+        System.out.println("The character for ASCII value " + asciiValue1 + " is: " + character1);
+    
+        int asciiValue2 = 97; 
+        char character2 = (char) asciiValue2;
+        System.out.println("The character for ASCII value " + asciiValue2 + " is: " + character2);
+   }
+}
