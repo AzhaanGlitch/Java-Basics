4c5b8c122a25b1671567221266479b42f4178110
Azhaan Ali Siddiqui <azhaanalisiddiqui15@gmail.com>
2025-11-24 20:39:07 +0530

    area of circle java

---
+++ ArOfCir.java
@@ -0,0 +1,9 @@
+class ArOfCir {
+        public static void main(String[] args) {
+        int r = 28;
+        double area = 3.14 * r * r;
+        double circumference = 2 * 3.14 * r;
+        System.out.println("The area of circle with radius " + r + " is = " + area);
+        System.out.println("The circumference of circle with radius " + r + " is = " + circumference);
+    }
+}
