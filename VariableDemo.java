57af012f599b2dd2c95362e35de4e1bc1a44883c
Azhaan Ali Siddiqui <azhaanalisiddiqui15@gmail.com>
2025-11-25 14:46:07 +0530

    varible demo java

---
+++ VariableDemo.java
@@ -0,0 +1,31 @@
+public class VariableDemo {
+    public static void main(String[] args) {
+        byte age = 28;
+        short year = 2025;
+        int population = 150000;
+        long bigNumber = 9876543210L;
+
+        float temperature = 98.6f;
+        double pi = 3.1415926535;
+
+        char grade = 'A';
+
+        boolean isJavaFun = true;
+
+        String greeting = "Hello, Java World!";
+
+        System.out.println("Primitive Data Types:");
+        System.out.println("Byte (age): " + age);
+        System.out.println("Short (year): " + year);
+        System.out.println("Int (population): " + population);
+        System.out.println("Long (bigNumber): " + bigNumber);
+        System.out.println("Float (temperature): " + temperature);
+        System.out.println("Double (pi): " + pi);
+        System.out.println("Char (grade): " + grade);
+        System.out.println("Boolean (isJavaFun): " + isJavaFun);
+        System.out.println("");
+
+        System.out.println("Non-Primitive Data Type:");
+        System.out.println("String (greeting): " + greeting);
+    }
+}
