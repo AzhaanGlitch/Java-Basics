ef0eb997c9bf1c43152820f77ce2a3c66a589ac8
Azhaan Ali Siddiqui <azhaanalisiddiqui15@gmail.com>
2025-11-25 14:43:01 +0530

    sum natural number java

---
+++ SumNaturalNumbers.java
@@ -0,0 +1,9 @@
+class SumNaturalNumbers {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 1; i <= 10; i++) {
+            sum += i;
+        }
+        System.out.println("The sum of first 10 natural numbers is : " + sum);
+    }
+}
